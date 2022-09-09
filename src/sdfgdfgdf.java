4afb0ed13525e4b4c052a10d5c25a49719aea33c
Kemal Asci <kemalasci01.com>
2022-09-09 09:31:24 +0200

    Initial commit

---
+++ src/sdfgdfgdf.java
@@ -0,0 +1,23 @@
+public class sdfgdfgdf {
+    public static void main(String[] args) {
+        System.out.println("södfgdfgkdfjg");
+    }
+
+
+    public static void adsfasdf(){
+
+
+        System.out.println("dfsadefdsfsdfsdfsdf");
+
+
+    }
+
+
+
+    public void sdfsdfdsa(){
+
+        System.out.println("djfsdkjfdskf");
+
+
+    }
+}
